05db49b2234695b5ce323ebea35d8bbf30a2b230
Maciej Lorens <mckl@poczta.fm>
2013-02-17 23:26:53 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrazyProjects::Application.config.secret_token = 'bae43acfb9173ba0b25c6baba121018668a122ed9a0bdbf6457c93fd89df1c57f7b7b1594d887f2609a2ce016ae8dae26ebe61218afca78867ce8073902c1e1e'
